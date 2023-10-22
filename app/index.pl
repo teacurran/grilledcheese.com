6cfb911a00517a439b79cc09a60e665eccd8da70
Terrence Curran <tcurran@approachingpi.com>
2023-10-22 18:55:48 -0400

    re-added index.pl

---
+++ app/index.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+require ("application.pl");
+
+$pageslocation = "/home/appi/grilledcheese.com/static/updates.txt";
+$main_template_file = "${basedir}${template_dir}${page_template}";
+
+
+undef $/;
+open (TEMPLATEFILE,"$main_template_file");
+ $template = <TEMPLATEFILE>;
+close(TEMPLATEFILE);
+
+($header, $footer) = split(/<!--- %MAIN% --->/, $template);
+
+open (STATIC,"$pageslocation");
+$maintext = <STATIC>;
+close(STATIC);
+
+$/ = "\n";
+($title_var) = ($maintext =~ /(.*?)\n/);
+$maintext =~ s/(.*?)\n//;
+
+
+$header =~ s/<!--- %TITLE% --->/$title_var/g;
+$footer =~ s/<!--- %QUOTE% --->/$hits_val/g;
+
+print "Content-type: text/html\n\n";
+print "$header\n";
+print "$maintext\n";
+print "$footer\n";
